393d2064d104a669e10de73fced0d6d67d650193
Pallavi Goel <pallavigoel2610@gmail.com>
2023-09-13 07:13:06 +0000

    start the game coding

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+print("Welcome to the Hangman Game")
+print("*****************************")
+a=input("Your name plz : ")
+print(a) 
