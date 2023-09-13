eb9e8fe875678f2d8ecc703ffef5a43df1b7e6c2
PallaviGoel1 <>
2023-09-13 12:32:43 +0000

    define a function

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def hangman():
+    
+    word = random.choice(wordsforhangman.word_choice)
+    turn = 7
+    guessmade = ''
+    valid_entry = set("abcdefghijklmnopqrstuvwxyz")
 
 
 print("Welcome to the Hangman Game")
