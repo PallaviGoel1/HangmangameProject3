70b94cf66bf92aa710509688a8fc3dd1006b43b7
PallaviGoel1 <>
2023-09-19 20:52:11 +0000

    try to all game rules in one print command

--- run.py
+++ run.py
@@ -185,6 +185,6 @@ print("---------------------------------------\n")
 print(Fore.YELLOW + "1-Start the game with your enter name\n""2-You need to guess the word one at a time\n"
 "3-Enter the small letter and press the enter key\n"
 "4-If Your small letter is right,Your letter will show on the correct place\n" 
-"5-If your small letter is wrong,Hangman visible and show,step by step\n")
+"5-If your small letter is wrong,Hangman visible and show,step by step\n"
 "6-keep write until the correct word or you exit the game\n")
 main()
