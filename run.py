1afb153697ede243b7a921c74647c5c28c7c2780
PallaviGoel1 <>
2023-09-20 18:11:29 +0000

    upate all

--- run.py
+++ run.py
@@ -191,7 +191,7 @@ def show_message():
     print(Fore.RED + "|                   ")
     print(Fore.RED + "|                  ")
     print(Fore.RED + "-------------------")
-    time.sleep(1)
+    time.sleep(1.5)
     # imitialalize colorama
     cl.init()
     # clear the terminal screen
